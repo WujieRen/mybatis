727fb5d11daf16010e4307f5d1dc5f73a79f9737
WujieRen <rwj005@outlook.com>
2018-05-13 15:20:20 +0800

    TypeAliases元素使用。

--- mybatisStudy/src/test/java/TestMyBatis.java
+++ mybatisStudy/src/test/java/TestMyBatis.java
@@ -1,4 +1,5 @@
 import com.mybatis.model.User;
+import com.mybatis.util.MyBatisUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -7,6 +8,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 /**
  * Created by renwujie on 2018/05/13 at 10:19
@@ -33,5 +35,24 @@ public class TestMyBatis {
         int count = session.selectOne("com.mybatis.model.User.getUserCount");
         //Assert.assertEquals(10, count);//断言
         System.out.println(count);
+    }
+
+    @Test
+    public void getUserList() {
+        //System.out.println(User.class.getName());
+        SqlSession session = null;
+        try {
+            session = MyBatisUtil.getSession();
+            //TODO:TypeAliases的作用是在.xml间能生效，这儿可以这么用是因为userMapper.xml中的namespace的名字和User的包名相同。
+            List<User> userList = session.selectList(User.class.getName()+".getAllUser");
+            //List<User> userList = session.selectList(User.class.getName()+"s.getAllUser");
+            for(User user : userList) {
+                System.out.println(user.getId() + " --- " + user.getUsername());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           MyBatisUtil.closeSession(session);
+        }
     }
 }
